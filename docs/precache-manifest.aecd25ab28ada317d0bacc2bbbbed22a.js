e281547bd17c766340e48cb382dbb449269805b2
Automedon <42299107+Automedon@users.noreply.github.com>
2020-09-25 20:07:40 +0700

    Homepage 💱

---
+++ docs/precache-manifest.aecd25ab28ada317d0bacc2bbbbed22a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbf710b469cc0235d9bd36d8ef67e40",
+    "url": "/Currency-Converter/index.html"
+  },
+  {
+    "revision": "aa5638eac791f305745e",
+    "url": "/Currency-Converter/static/css/main.c05de8cc.chunk.css"
+  },
+  {
+    "revision": "674568ef5b556458214c",
+    "url": "/Currency-Converter/static/js/2.13d7620f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency-Converter/static/js/2.13d7620f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5638eac791f305745e",
+    "url": "/Currency-Converter/static/js/main.6fe4b199.chunk.js"
+  },
+  {
+    "revision": "a4e4a378852c8616af65",
+    "url": "/Currency-Converter/static/js/runtime-main.2995fc3e.js"
+  }
+]);
